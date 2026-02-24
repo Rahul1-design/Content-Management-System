2670dc9443292b4bc0952cbbe2fff32cd8ba2245
Rahul Shrestha <rahulshrestha3132@gmail.com>
2026-02-24 12:48:12 +0545

    Add PostPage component to fetch and display individual posts

---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,22 @@
+async function getPost(slug: string) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/${slug}`, { cache: 'no-store' })
+    const data = await res.json();
+    return data.post
+}
+
+export default async function PostPage({ params }: { params: { slug: string } }) {
+    const post = await getPost(params.slug)
+
+    if (!post) return <div className="text-red-700 font-semibold text-4xl ">Post not found</div>
+
+    return (
+        <article className="max-w-3xl mx-auto p-8">
+            <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
+            <div className="text-gray-500 mb-8">
+                By {post.author?.name} • {new Date(post.createdAt).toLocaleDateString()}
+            </div>
+            <div className="prose max-w-none">{post.content}</div>
+        </article>
+    )
+
+}
