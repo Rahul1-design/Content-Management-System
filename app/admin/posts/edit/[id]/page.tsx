e2af133f83c77e7b69ba7252da16c211dc08c6fc
Rahul Shrestha <rahulshrestha3132@gmail.com>
2026-03-09 15:15:56 +0545

    adding the edit page for the post

---
+++ app/admin/posts/edit/[id]/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+
+export default async function editPosts({ params }: { params: { id: string } }) {
+
+}
